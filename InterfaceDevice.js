50b6017c00980b95ed95fba4e048e155417497cc
Gerard <gerar231@gmail.com>
2018-10-20 12:23:45 -0700

    Created react app.

--- InterfaceDevice.js
+++ InterfaceDevice.js
@@ -28,9 +28,7 @@ class InterfaceDevice {
 const request = require('request');
 
 request('', {json: true}, (err, res, body) => {
-    if (err) {return console.log(err); }
-
-    console.log(body.url);
-
-    console.log(body.explanation);
-})
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+}); 
