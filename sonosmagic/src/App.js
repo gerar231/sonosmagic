4642cfb1f94d1295d07f094c2d72c4a40ba12782
Gerard <gerar231@gmail.com>
2018-10-23 06:42:40 -0700

    Created main view, scaffold for other pages in place.

--- sonosmagic/src/App.js
+++ sonosmagic/src/App.js
@@ -1,34 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { createAuthorize } from './sonosAuthorize';
+import './app.css';
+import './common.css';
+import logo from './sonos-family-logo.png'
+import {createAuthorize} from './sonosAuthorize.js';
 
 class App extends Component {
-  renderButton() {
-    return <LoginButton/>;
-  }
   render() {
     return (
       <div className="App">
-        <LoginButton/>  
+        <Navigation pageId="WelcomeView"/>
       </div>
     );
   }
 }
 
-class LoginButton extends Component {
-  handleClick = () => {
-    // call sonos authorization here
-    // if authorization successful return some information
-    // to the console and send a window alert that says success.
-    createAuthorize();
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.getView = this.getView.bind(this);
+    this.setView = this.setView.bind(this);
+    this.state = {currentView: this.getView(props.pageId)};
+  }
+  getView(pageId) {
+    switch(pageId) {
+      case 'WelcomeView':
+        return (<WelcomeView viewControl={this.setView}/>);
+      case 'ControlSelectView':
+        return (<ControlSelectView viewControl={this.setView}/>);
+      case 'GroupsSelectView':
+        return (<GroupsSelectView viewControl={this.setView}/>);
+      case 'MonitorView':
+        return (<MonitorView viewControl={this.setView}/>);
+      default: 
+        return (<WelcomeView viewControl={this.setView}/>);
+    }
+  }
+  setView(pageId) {
+    this.setState({currentView: this.getView(pageId)});
   }
   render() {
     return (
-      <button onClick={this.handleClick}>
-          Login to SONOS
-      </button>
+      <div className="spacing-height-full spacing-width-full">
+        {this.state.currentView}
+      </div>
+    )
+  }
+}
+class ActionButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: props.name};
+    this.handleClick = props.onClick;
+  }
+  render() {
+    return (
+      <a className="button white large small-6" href="#" onClick={this.handleClick}>
+         {this.state.text} 
+      </a>
     );
+  }
+}
+
+class View extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {viewControl: props.viewControl};
+    this.state.viewControl = this.state.viewControl.bind(this);
+  }
+}
+
+class WelcomeView extends View {
+  fireAlert() {
+    window.alert("fired alert.");
+  }
+  render() {
+    return (
+      <div className="bg-black section spacing-height-full spacing-width-full vert-align-parent">
+        <div className="row vert-align-child">
+          <div className="columns small-12 small-centered text-center">
+            <div className="section-header">
+              <h2 className="white"> SONOS </h2>
+              <h2 className="white"> + </h2>
+              <h2 className="white"> FAMILY </h2>
+            </div>
+            <div className="row">
+              <div className="columns">
+                <img src={logo} className="vert-padding-6vw small-3" alt="?"/>
+              </div>
+            </div>
+            <div className="row">
+              <ActionButton name="LOGIN" onClick={(e) => this.fireAlert(e)}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ControlSelectView extends View {
+  render() {
+    return (
+      <div>
+      <div className="row">
+        <ActionButton name="NextPage" onClick={this.state.viewControl('GroupsSelectView')}/>
+      </div>
+      </div>
+    )
+  }
+}
+
+class GroupsSelectView extends View {
+  render() {
+    return(
+      <div>
+        this is view groups
+      </div>
+    );
+  }
+}
+
+class MonitorView extends View {
+  render() {
+    return(
+      <div>
+        this is Monitor
+      </div>
+    )
   }
 }
 
--- sonosmagic/src/sonosAuthorize.js
+++ sonosmagic/src/sonosAuthorize.js
@@ -1,71 +1,11 @@
 export const createAuthorize = () => {
-        var client_id = "3ca08a69-9582-48b9-9479-76a34805704e";
-        var redirect_uri = "http://localhost:3000";
-        var response_type = "code";
-        var scope = "playback-control-all";
-        var state = "RANDOM-STATE";
+    var client_id = "3ca08a69-9582-48b9-9479-76a34805704e";
+    var redirect_uri = "http://localhost:3000";
+    var response_type = "code";
+    var scope = "playback-control-all";
+    var state = "RANDOM-STATE";
 
-        var loginUrl = "https://api.sonos.com/login/v3/oauth?client_id=" + client_id + "&response_type=" + response_type + "&state=" + state + "&scope=" + scope + "&redirect_uri=" + redirect_uri;
+    var loginUrl = "https://api.sonos.com/login/v3/oauth?client_id=" + client_id + "&response_type=" + response_type + "&state=" + state + "&scope=" + scope + "&redirect_uri=" + redirect_uri;
 
-        window.open(loginUrl, "_self");
-
-        // OAuth1.0 - 3-legged server side flow (Twitter example)
-        // step 1
-        var qs = require('querystring');
-        var oauth = {
-                callback: 'http://localhost:3000',
-                consumer_key: CONSUMER_KEY,
-                consumer_secret: CONSUMER_SECRET
-        };
-        request.post({url: url, oauth: oauth}, function (e, r, body) {
-            // Ideally, you would take the body in the response
-            // and construct a URL that a user clicks on (like a sign in button).
-            // The verifier is only available in the response after a user has
-            // verified with twitter that they are authorizing your app.
-
-            // step 2
-            var req_data = qs.parse(body)
-            var uri = 'https://api.twitter.com/oauth/authenticate' +
-                '?' + qs.stringify({
-                    oauth_token: req_data.oauth_token
-                })
-            // redirect the user to the authorize uri
-
-            // step 3
-            // after the user is redirected back to your server
-            var auth_data = qs.parse(body),
-                oauth = {
-                    consumer_key: CONSUMER_KEY,
-                    consumer_secret: CONSUMER_SECRET,
-                    token: auth_data.oauth_token,
-                    token_secret: req_data.oauth_token_secret,
-                    verifier: auth_data.oauth_verifier
-                },
-                url = 'https://api.twitter.com/oauth/access_token';
-            request.post({
-                url: url,
-                oauth: oauth
-            }, function (e, r, body) {
-                // ready to make signed requests on behalf of the user
-                var perm_data = qs.parse(body),
-                    oauth = {
-                        consumer_key: CONSUMER_KEY,
-                        consumer_secret: CONSUMER_SECRET,
-                        token: perm_data.oauth_token,
-                        token_secret: perm_data.oauth_token_secret
-                    },
-                    url = 'https://api.twitter.com/1.1/users/show.json',
-                    qs = {
-                        screen_name: perm_data.screen_name,
-                        user_id: perm_data.user_id
-                    };
-                request.get({
-                    url: url,
-                    oauth: oauth,
-                    qs: qs,
-                    json: true
-                }, function (e, r, user) {
-                    console.log(user)
-                })
-            })
-        })
+    window.open(loginUrl, "_self");
+}
