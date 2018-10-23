c56044b2c7cff00dd1e06e945f90cde4c9d84bbd
Gerard <gerar231@gmail.com>
2018-10-23 09:18:00 -0700

    Finished with UI through selecting speakers.

--- sonosmagic/src/App.js
+++ sonosmagic/src/App.js
@@ -4,13 +4,13 @@ import './app.css';
 import './common.css';
 import logo from './sonos-family-logo.png'
 import {createAuthorize} from './sonosAuthorize.js';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+/* import { BrowserRouter as Router, Route, Link } from "react-router-dom"; */
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WelcomeView/>
+        <ControlSelectView/>
       </div>
     );
   }
@@ -70,20 +70,69 @@ class WelcomeView extends View {
 class ControlSelectView extends View {
   render() {
     return (
-      <div>
-      <div className="row">
-        <ActionButton name="NextPage" onClick={this.state.viewControl('GroupsSelectView')}/>
-      </div>
+      <div className="bg-black section spacing-height-full spacing-width-full vert-align-parent"> 
+        <div className="row vert-align-child">
+          <div className="columns small-12 small-centered text-center">
+            <div className="row">
+            <div className="large-icon">
+              <span className="icon-home-music white"></span>
+            </div>
+            </div>
+            <div className="row vert-padding-6vw">
+               <ActionButton name="Connect" onClick={(e) => window.alert("alert fired", e)}/> 
+            </div>
+            <div className="row">
+              <ActionButton name="Monitor" onClick={(e) => window.alert("alert fired", e)}/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-class GroupsSelectView extends View {
+class GroupButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: props.text}
+  }
   render() {
+    return (
+    <a href="#" className="button large small-9" href="#" onClick={this.handleClick}>
+        {this.state.text}
+    </a>
+    );
+  }
+}
+
+class GroupsSelectView extends View {
+  constructor(props) {
+    super(props);
+    // get available speakers
+    // update state of the monitor object 
+    // on click handler for each tile here
+    this.state = {groups: ["Living Room", "Study", "Kitchen", "Bedroom"]};
+  }
+   
+  render() {
+    var buttons = this.state.groups.map(function(name) {
+                    return <div className="row spacing-pa-l">
+                              <GroupButton text={name}/>
+                            </div>;
+                  });
     return(
-      <div>
-        this is view groups
+      <div className="bg-gray-light spacing-height-full spacing-width-full vert-align-parent">
+        <div className="row vert-align-child">
+          <div className="columns small-12 small-center text-center">
+          <h2 className="title">
+            Where do you want to monitor?
+          </h2>
+          <div className="large-icon">
+            <span className="icon-mic"></span>
+          </div>
+           {buttons}
+          </div>
+        </div>
       </div>
     );
   }
